8fa92a4c77cca1debddecaa3cca357f848b7b987
Quentin Mathé <quentin.mathe@gmail.com>
2007-11-23 15:24:23 +0000

    r2329@macbook-de-quentin-mathe:  qmathe | 2007-11-22 14:24:26 +0100  Some preliminary documentation about the event handling in the layout item tree.
    git-svn-id: http://svn.gna.org/svn/etoile/branches/qmathe/Container@2653 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- ETLayoutItem+Events.h
+++ ETLayoutItem+Events.h
@@ -43,6 +43,67 @@
 // TODO: Refactor this category in a pluggable event handler object. 
 // A new ETInteraction class or class hierachy should be introduced.
 
+/** Event Handling in the Layout Item Tree 
+
+	What follows is only valid when the layout item tree is rendered by 
+	EtoileUI/AppKit rendering backend (see ETEtoileUIRender).
+	The events are routed through the AppKit view hierarchy to the proper child
+	item of the layout item matching the view that replies YES to -hitTest. If 
+	the view has no corresponding layout item and returns YES, the event 
+	handling remains on AppKit side (following AppKit event model). If this 
+	view returns NO, then -hitTest is done the superview which may have a 
+	matching layout item or not. 
+	ETView class and subclasses replies YES to -hitTest unlike NSView, so the
+	event they received are always routed to their corresponding layout item.
+	Basic vent handling methods in ETLayoutItem reuses the name of their 
+	equivalent NSResponder methods plus an additional method keywoard 'on:'.
+	Here is an example:
+	- (void) mouseDown: (NSEvent *)event in Responder
+	becomes
+	- (void) mouseDown: (NSEvent *)event on: (id)item in ETLayoutItem.
+	Presently these methods are declared on ETLayoutItem, although in future
+	they will be moved to a separate ETEventHandler class making possible to
+	to changing event handling at runtime by plugging another event handler.
+	This will help to support multiple interaction modes very easily as needed 
+	by many graphics applications and to implement custom mouse tracking 
+	(radio buttons, menus, custom controls etc.)
+	
+	When a layout item receives an event, it looks up for the higher ancestor 
+	item that wants to preempt the event. For example, if the root item has an
+	outline layout active instead of the common UI layout, then the root item
+	will handles the event in all cases. Even if the item which receives the
+	event is ten levels below or more. This rule is important because it allows
+	transparent pick and drop: if the item receiving the event has a parent 
+	which uses a table layout, a drag should be handled according to the drag 
+	semantic defined by table layout, but this doesn't hold if this table
+	layout is invisible because some higher ancestor item in the tree uses an
+	outline layout at this time.
+	Most of time there are no ancestor items which want to preempt the event, 
+	in this case
+	
+	Here is a list of layouts that preempts events:
+	- ETTableLayout
+	- ETOutlineLayout
+	- ETBrowserLayout
+	- ETModelViewLayout
+	- any layouts which uses a layout view (injected in a container when you 
+	  call -setLayout: on its layout item).
+	  
+	Layouts which don't preempt events:
+	- ETFlowLayout
+	- ETStackLayout
+	- ETLineLayout
+	- ETFreeLayout
+	- ETUILayout
+	
+	If you use a source with your container and you implement mutation source 
+	methods, you don't need to worry about pick and drop interaction handling.
+	ETLayoutItemGroup mutation methods like -addItem:, -removeItem: etc. hides 
+	all the details involved by dealing with a source when one is set. These 
+	methods takes care of finding whether the item is provided by a source or 
+	not by looking up in the layout item tree.
+    */
+
 @interface ETLayoutItem (Events) <ETEventHandler>
 
 - (BOOL) allowsDragging;
