c5d32b42a46e3824c2ae69c0bd70457d941680f5
Quentin Mathé <quentin.mathe@gmail.com>
2008-06-02 21:57:55 +0000

    Fixed incorrect tests for -renderView of ETLayoutItemBuilder
    git-svn-id: http://svn.gna.org/svn/etoile/branches/qmathe/Container@3146 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/test_ETLayoutItemBuilder.m
+++ Tests/test_ETLayoutItemBuilder.m
@@ -43,6 +43,11 @@
 @interface ETEtoileUIBuilder (UnitKitTests) <UKTest>
 @end
 
+/* NSView subclass for testing -renderView */
+@interface CustomView : NSView { }
+@end
+@implementation CustomView
+@end
 
 @implementation ETEtoileUIBuilder (UnitKitTests)
 
@@ -81,7 +86,7 @@
 {
 	id view = AUTORELEASE([[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
 	id subview0 = AUTORELEASE([[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
-	id subview1 = AUTORELEASE([[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
+	id subview1 = AUTORELEASE([[CustomView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
 	id subview00 = AUTORELEASE([[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
 	
 	id subview01 = AUTORELEASE([[ETView alloc] initWithFrame: NSMakeRect(0, 0, 100, 50)]);
@@ -103,12 +108,15 @@
 	id rootItem = [[ETEtoileUIBuilder builder] renderView: view];
 	id childItem = nil;
 	
-	UKObjectsSame(view, [rootItem view]);
+	UKObjectsNotSame(view, [rootItem view]);
 	UKIntsEqual([[rootItem items] count], 2);
-	UKObjectsSame([[rootItem itemAtIndex: 0] view], subview0);
+	/* NSView are turned into containers but NSView subclasses aren't */
+	UKObjectsNotSame(subview0, [[rootItem itemAtIndex: 0] view]);
+	UKObjectKindOf([[rootItem itemAtIndex: 0] view], ETContainer);
+	//UKObjectKindOf(ETContainer, [[rootItem itemAtIndex: 0] view]);
 	UKObjectsSame([[rootItem itemAtIndex: 1] view], subview1);
 	childItem = [rootItem itemAtIndex: 0];
-	UKObjectsSame([[childItem itemAtIndex: 0] view], subview00);
+	UKObjectsNotSame([[childItem itemAtIndex: 0] view], subview00);
 	
 	UKFalse([(ETLayoutItem *)[childItem itemAtIndex: 1] isGroup]); // ETView item check
 	childItem = [childItem itemAtIndex: 2];
