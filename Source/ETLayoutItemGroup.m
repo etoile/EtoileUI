etoile/EtoileUI

commit 3de6911c7d1beab7f52d211cdc654393ba55960d

Deprecated more selection related methods in ETContainer. Moved -setSelectionIndexPaths: notification and redisplay logic into the identically named ETLayoutItemGroup method.
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4116 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETLayoutItemGroup.m
@@ -1376,10 +1376,23 @@ length equal one. */
 {
 	[self applySelectionIndexPaths: [NSMutableArray arrayWithArray: indexPaths] 
 	                relativeToItem: self];
+
+	// FIXME: Rename the notification to ETLayoutItemGroupSelectionDidChangeNotification
+	/* Finally propagate changes by posting notification */
+	NSNotification *notif = [NSNotification 
+		notificationWithName: ETContainerSelectionDidChangeNotification object: self];
+	
+	if ([[[self container] delegate] respondsToSelector: @selector(containerSelectionDidChange:)])
+		[[[self container] delegate] containerSelectionDidChange: notif];
+	
+	[[NSNotificationCenter defaultCenter] postNotification: notif];
 	
 	/* For opaque layouts that may need to keep in sync the selection state of 
 	   their custom UI. */
 	[[self layout] selectionDidChangeInLayoutContext];
+	
+	/* Reflect selection change immediately */
+	[[self supervisorView] display]; // TODO: supervisorView is probably not the best choice...
 }
 
 /** Returns the selected child items belonging to the receiver. 

Headers/ETContainer.h
@@ -136,9 +136,6 @@
 
 /* Selection */
 
-- (NSArray *) selectedItemsInLayout;
-- (NSArray *) selectionIndexPaths;
-- (void) setSelectionIndexPaths: (NSArray *)indexPaths;
 - (BOOL) allowsMultipleSelection;
 - (void) setAllowsMultipleSelection: (BOOL)multiple;
 - (BOOL) allowsEmptySelection;
@@ -330,6 +327,9 @@ extern NSString *ETLayoutItemPboardType;
 
 /* Selection */
 
+- (NSArray *) selectedItemsInLayout;
+- (NSArray *) selectionIndexPaths;
+- (void) setSelectionIndexPaths: (NSArray *)indexPaths;
 - (void) setSelectionIndexes: (NSIndexSet *)selection;
 - (NSMutableIndexSet *) selectionIndexes;
 - (void) setSelectionIndex: (unsigned int)index;

Source/ETContainer.m
@@ -1048,37 +1048,6 @@ NSString *ETLayoutItemPboardType = @"ETLayoutItemPboardType"; // FIXME: replace 
 
 /* Selection */
 
-/** See -[ETLayoutItemGroup selectedItemsInLayout] */
-- (NSArray *) selectedItemsInLayout
-{
-	return [(ETLayoutItemGroup *)[self layoutItem] selectedItemsInLayout];
-}
-
-/** See -[ETLayoutItemGroup selectionIndexPaths] */
-- (NSArray *) selectionIndexPaths
-{
-	return [(ETLayoutItemGroup *)[self layoutItem] selectionIndexPaths];
-}
-
-/** See -[ETLayoutItemGroup setSelectionIndexPaths] */
-- (void) setSelectionIndexPaths: (NSArray *)indexPaths
-{
-	[(ETLayoutItemGroup *)[self layoutItem] setSelectionIndexPaths: indexPaths];
-	
-	// FIXME: Move this code into -[ETLayoutItemGroup setSelectionIndexPaths:]
-	/* Finally propagate changes by posting notification */
-	NSNotification *notif = [NSNotification 
-		notificationWithName: ETContainerSelectionDidChangeNotification object: self];
-	
-	if ([[self delegate] respondsToSelector: @selector(containerSelectionDidChange:)])
-		[[self delegate] containerSelectionDidChange: notif];
-
-	[[NSNotificationCenter defaultCenter] postNotification: notif];
-	
-	/* Reflect selection change immediately */
-	[self display];
-}
-
 - (BOOL) allowsMultipleSelection
 {
 	return _multipleSelectionAllowed;
@@ -1539,6 +1508,24 @@ NSString *ETLayoutItemPboardType = @"ETLayoutItemPboardType"; // FIXME: replace 
 - (NSArray *) items
 {
 	return [(ETLayoutItemGroup *)[self layoutItem] items];
+}
+
+/** See -[ETLayoutItemGroup selectedItemsInLayout] */
+- (NSArray *) selectedItemsInLayout
+{
+	return [(ETLayoutItemGroup *)[self layoutItem] selectedItemsInLayout];
+}
+
+/** See -[ETLayoutItemGroup selectionIndexPaths] */
+- (NSArray *) selectionIndexPaths
+{
+	return [(ETLayoutItemGroup *)[self layoutItem] selectionIndexPaths];
+}
+
+/** See -[ETLayoutItemGroup setSelectionIndexPaths] */
+- (void) setSelectionIndexPaths: (NSArray *)indexPaths
+{
+	[(ETLayoutItemGroup *)[self layoutItem] setSelectionIndexPaths: indexPaths];
 }
 
 - (void) setSelectionIndexes: (NSIndexSet *)indexes

commit 6a30e2561a2388f6de8b2552d10834987bd84a7e

Fixed a missing call to -didChangeSelection. This mistake was introduced with r4116.
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4538 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETLayoutItemGroup.m
@@ -1534,6 +1534,7 @@ Posts an ETItemGroupSelectionDidChangeNotification. */
 	/* For opaque layouts that may need to keep in sync the selection state of 
 	   their custom UI. */
 	[[self layout] selectionDidChangeInLayoutContext];
+	[self didChangeSelection];
 
 	/* Reflect selection change immediately */
 	[[self supervisorView] display]; // TODO: supervisorView is probably not the best choice...