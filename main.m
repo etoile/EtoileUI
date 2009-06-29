eb65014aced2f36a069be7c95ceeb22b118473f4
Quentin Mathé <quentin.mathe@gmail.com>
2009-06-29 14:29:08 +0000

    Updated main.m used by every examples (or almost) to rely on ETApplicationMain()
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4859 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- main.m
+++ main.m
@@ -37,5 +37,5 @@
 int main(int argc, char *argv[])
 {
 	NSZombieEnabled = YES;
-    return NSApplicationMain(argc,  (const char **) argv);
+    return ETApplicationMain(argc,  (const char **) argv);
 }
