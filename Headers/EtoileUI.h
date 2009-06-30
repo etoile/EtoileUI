bf1a8a70ee1d80cabffd1dc41f40fb0a1cb561ef
Quentin Mathé <quentin.mathe@gmail.com>
2009-06-30 15:05:16 +0000

    Sorted the header import list in EtoileUI umbrella header
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4862 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/EtoileUI.h
+++ Headers/EtoileUI.h
@@ -8,68 +8,59 @@
 	License: Modified BSD (see COPYING)
  */
 
-#import <EtoileUI/ETCompatibility.h>
-#import <EtoileUI/ETGeometry.h>
-
-#import <EtoileUI/NSObject+EtoileUI.h>
-#import <EtoileUI/NSView+Etoile.h>
-#import <EtoileUI/NSWindow+Etoile.h>
-#import <EtoileUI/NSImage+Etoile.h>
 #import <EtoileUI/Controls+Etoile.h>
-#import <EtoileUI/ETInspecting.h>
-
+#import <EtoileUI/ETActionHandler.h>
 #import <EtoileUI/ETApplication.h>
-#import <EtoileUI/ETLayoutItemBuilder.h>
-#import <EtoileUI/ETPickboard.h>
-
-#import <EtoileUI/ETStyle.h>
-#import <EtoileUI/ETStyleGroup.h>
-#import <EtoileUI/ETShape.h>
+#import <EtoileUI/ETBrowserLayout.h>
+#import <EtoileUI/ETCompatibility.h>
+#import <EtoileUI/ETComputedLayout.h>
 #import <EtoileUI/ETContainer.h>
 #import <EtoileUI/ETController.h>
 #import <EtoileUI/ETDecoratorItem.h>
-#import <EtoileUI/ETLayoutItem.h>
-#import <EtoileUI/ETActionHandler.h>
-#import <EtoileUI/ETLayoutItem+Reflection.h>
-#import <EtoileUI/ETLayoutItem+Scrollable.h>
 #import <EtoileUI/ETEvent.h>
-#import <EtoileUI/ETLayoutItemGroup.h>
-#import <EtoileUI/ETLayoutItemGroup+Mutation.h>
-#import <EtoileUI/ETLayoutItem+Factory.h>
-#import <EtoileUI/ETLayer.h>
-#import <EtoileUI/ETSelectionAreaItem.h>
-#import <EtoileUI/ETWindowItem.h>
-#import <EtoileUI/ETUIItem.h>
-#import <EtoileUI/ETUIItemFactory.h>
-#import <EtoileUI/ETLayout.h>
-#import <EtoileUI/ETLayoutLine.h>
-
+#import <EtoileUI/ETFlowLayout.h>
+#import <EtoileUI/ETFreeLayout.h>
+#import <EtoileUI/ETGeometry.h>
+#import <EtoileUI/ETIconLayout.h>
+#import <EtoileUI/ETInspecting.h>
+#import <EtoileUI/ETInspector.h>
 #import <EtoileUI/ETInstrument.h>
 #import <EtoileUI/ETInstruments.h>
-#import <EtoileUI/ETPaintBucketTool.h>
-#import <EtoileUI/ETSelectTool.h>
-#import <EtoileUI/ETPickDropCoordinator.h>
-
-#import <EtoileUI/ETComputedLayout.h>
-#import <EtoileUI/ETFlowLayout.h>
+#import <EtoileUI/ETLayer.h>
+#import <EtoileUI/ETLayout.h>
+#import <EtoileUI/ETLayoutItem+Factory.h>
+#import <EtoileUI/ETLayoutItem+Reflection.h>
+#import <EtoileUI/ETLayoutItem+Scrollable.h>
+#import <EtoileUI/ETLayoutItem.h>
+#import <EtoileUI/ETLayoutItemBuilder.h>
+#import <EtoileUI/ETLayoutItemGroup+Mutation.h>
+#import <EtoileUI/ETLayoutItemGroup.h>
+#import <EtoileUI/ETLayoutLine.h>
 #import <EtoileUI/ETLineLayout.h>
-#import <EtoileUI/ETStackLayout.h>
-#import <EtoileUI/ETTemplateItemLayout.h>
-#import <EtoileUI/ETIconLayout.h>
-
-#import <EtoileUI/ETTableLayout.h>
-#import <EtoileUI/ETOutlineLayout.h>
-#import <EtoileUI/ETBrowserLayout.h>
-#import <EtoileUI/FSBrowserCell.h>
-
-#import <EtoileUI/ETPaneLayout.h>
-
-#import <EtoileUI/ETFreeLayout.h>
-
 #import <EtoileUI/ETObjectBrowserLayout.h>
-#import <EtoileUI/ETViewModelLayout.h>
+#import <EtoileUI/ETOutlineLayout.h>
+#import <EtoileUI/ETPaintBucketTool.h>
+#import <EtoileUI/ETPaneLayout.h>
+#import <EtoileUI/ETPickDropCoordinator.h>
+#import <EtoileUI/ETPickboard.h>
+#import <EtoileUI/ETSelectTool.h>
+#import <EtoileUI/ETSelectionAreaItem.h>
+#import <EtoileUI/ETShape.h>
+#import <EtoileUI/ETStackLayout.h>
+#import <EtoileUI/ETStyle.h>
+#import <EtoileUI/ETStyleGroup.h>
+#import <EtoileUI/ETTableLayout.h>
+#import <EtoileUI/ETTemplateItemLayout.h>
 #import <EtoileUI/ETTextEditorLayout.h>
-#import <EtoileUI/ETInspector.h>
+#import <EtoileUI/ETUIItem.h>
+#import <EtoileUI/ETUIItemFactory.h>
+#import <EtoileUI/ETViewModelLayout.h>
+#import <EtoileUI/ETWindowItem.h>
+#import <EtoileUI/FSBrowserCell.h>
+#import <EtoileUI/NSImage+Etoile.h>
+#import <EtoileUI/NSObject+EtoileUI.h>
+#import <EtoileUI/NSView+Etoile.h>
+#import <EtoileUI/NSWindow+Etoile.h>
 
 #ifdef COREOBJECT
 #import <EtoileUI/ETPersistencyController.h>
