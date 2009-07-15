53396fadb9e2a8ab9af7ca07830de2c65c172057
Quentin Mathé <quentin.mathe@gmail.com>
2009-07-15 11:16:45 +0000

    Implemented NSCopying in NSWindow
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4933 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/NSWindow+Etoile.m
+++ Source/NSWindow+Etoile.m
@@ -1,37 +1,9 @@
-/*  <title>NSWindow+Etoile</title>
-
-	NSWindow+Etoile.m
-	
-	<abstract>NSWindow additions.</abstract>
- 
+/*
 	Copyright (C) 2007 Quentin Mathe
- 
+
 	Author:  Quentin Mathe <qmathe@club-internet.fr>
 	Date:  December 2007
- 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License: Modified BSD (see COPYING)
  */
 
 #import <EtoileUI/NSWindow+Etoile.h>
@@ -76,6 +48,25 @@
 					       styleMask: windowStyle
 							 backing: NSBackingStoreBuffered
 							   defer: NO];
+}
+
+/** Returns a window copy of the receiver. 
+
+The content view and its whole subview tree is not copied, in other words the 
+new object is a shallow copy of the receiver.*/
+- (id) copyWithZone: (NSZone *)zone
+{
+	NSView *contentView = RETAIN([self contentView]);
+
+	[self setContentView: nil];
+
+	NSData *windowData = [NSKeyedArchiver archivedDataWithRootObject: self];
+	NSWindow *windowCopy = [NSKeyedUnarchiver unarchiveObjectWithData: windowData];
+
+	[self setContentView: contentView];
+	RELEASE(contentView);
+
+	return RETAIN(windowCopy);
 }
 
 - (void) setFrameSizeFromTopLeft: (NSSize)size
