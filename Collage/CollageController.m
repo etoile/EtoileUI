34277e1ca277edd1e5e1055dc4660c0b6a612c6b
Quentin Mathé <quentin.mathe@gmail.com>
2009-07-17 15:37:52 +0000

    Cleaned up the messy Collage example and updated it to use the latest API
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4952 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Collage/CollageController.m
+++ Collage/CollageController.m
@@ -1,76 +1,68 @@
 /*
-	CollageController.m
-	
-	An extensible graphics editor demonstrating EtoileUI as a toolkit to build 
-	structured graphics editor.
- 
 	Copyright (C) 2008 Quentin Mathe
- 
+
 	Author:  Quentin Mathe <qmathe@club-internet.fr>
 	Date:  December 2008
- 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License:  Modified BSD (see COPYING)
  */
 
 #import "CollageController.h"
-//#import <EtoileUI/ETInstruments.h>
-//#import <EtoileFoundation/ETModelDescription.h>
 
 
 @implementation CollageController
 
 - (void) applicationDidFinishLaunching: (NSNotification *)notif
 {
+	ETUIItemFactory *itemFactory = [ETUIItemFactory factory];
+
+	/* Shows the graphics editing related menu which contains commands like 
+	   'group', 'ungroup', 'send backward' etc. */
 	[[ETApp mainMenu] addItem: [ETApp arrangeMenuItem]];
 
-	mainItem = [ETLayoutItem itemGroup];
+	mainItem = [itemFactory itemGroup];
+	
+	/* Set up main item to behave like a very basic compound document editor */
+
+	// NOTE: Uncomment next line to test the example with a content coordinate 
+	// base located in the bottom left.
 	//[mainItem setFlipped: NO];
 	[mainItem setSize: NSMakeSize(500, 400)];
 	[mainItem setLayout: [ETFreeLayout layout]];
-	[[ETLayoutItem windowGroup] addItem: mainItem];
 
-	[mainItem addItem: [ETLayoutItem horizontalSlider]];
-	[mainItem addItem: [ETLayoutItem textField]];
-	[mainItem addItem: [ETLayoutItem labelWithTitle: @"Hello World!"]];
-	[mainItem addItem: [ETLayoutItem button]];
-	[mainItem addItem: [ETLayoutItem rectangle]];
-	[mainItem addItem: [ETLayoutItem oval]];
-	//[mainItem addItem: AUTORELEASE([[ETSelectionAreaItem alloc] init])];
+	/* Make mainItem visible by inserting it inside the window layer */
+
+	[[itemFactory windowGroup] addItem: mainItem];
+	
+	/* Insert a bit of everything as content (widgets and shapes) */
+
+	[mainItem addItem: [itemFactory horizontalSlider]];
+	[mainItem addItem: [itemFactory textField]];
+	[mainItem addItem: [itemFactory labelWithTitle: @"Hello World!"]];
+	[mainItem addItem: [itemFactory button]];
+	[mainItem addItem: [itemFactory rectangle]];
+	[mainItem addItem: [itemFactory oval]];
+	/* Selection rubber-band is a layout item too, which means we can use it 
+	   in the same way than other shape-based items... */
+	[mainItem addItem: AUTORELEASE([[ETSelectionAreaItem alloc] init])];
+
+	/* ... A less useless use case would be to replace the shape bound to it or 
+	   alter its shape as below. */
+
+	ETSelectTool *tool = [[mainItem layout] attachedInstrument];
+	[[[tool selectionAreaItem] style] setStrokeColor: [NSColor orangeColor]];
+
+	/* Give grid-like positions to items initially */
 
 	ETFlowLayout *flow = [ETFlowLayout layout];
 	[flow setItemSizeConstraintStyle: ETSizeConstraintStyleNone];
 	[(ETFreeLayout *)[mainItem layout] resetItemPersistentFramesWithLayout: flow];
-	//[mainItem setLayout: [ETFlowLayout layout]];
-	//[[mainItem layout] setAttachedInstrument: [ETMoveTool instrument]];
-	//[ETInstrument setMainInstrument: [ETSelectTool instrument]];
-	//[[ETPickboard localPickboard] showPickPalette];
-	//[mainItem inspect: nil];
-	[[ETLayoutItem windowGroup] inspect: nil];
-	//[[ETModelDescription modelDescription] view: nil];
+
+	/* Open an inspector that allows us to easily switch the instrument and the 
+	   layout in use */
+
+	[[itemFactory windowGroup] inspect: nil];
 	
-	//[ETApp explore: nil];
+	// FIXME: [ETApp explore: nil];
 }
 
 @end
