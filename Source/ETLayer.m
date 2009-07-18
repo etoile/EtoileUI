f2f575356048cad8778bbb4969ddafac7c0f0d22
Quentin Mathé <quentin.mathe@gmail.com>
2009-07-18 09:55:35 +0000

    Modified ETPickboard and ETWindowLayer to use guard clause in initializers and ETUIItemFactory rather the deprecated ETLayoutItem(Factory).
    Cleaned header imports and shortened license notices.
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@4962 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/ETLayer.m
+++ Source/ETLayer.m
@@ -1,49 +1,20 @@
-/*  <title>ETLayer</title>
-
-	ETLayer.m
-	
-	<abstract>Layer class models the traditional layer element, very common in 
-	Computer Graphics applications.</abstract>
- 
+/*
 	Copyright (C) 2007 Quentin Mathe
- 
+
 	Author:  Quentin Mathe <qmathe@club-internet.fr>
 	Date:  May 2007
- 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License:  Modified BSD (see COPYING)
  */
 
 #import <EtoileFoundation/Macros.h>
 #import "ETLayer.h"
 #import "ETApplication.h"
-#import "ETContainer.h"
-#import "ETCompatibility.h"
+#import "ETDecoratorItem.h"
 #import "ETInstruments.h"
-#import "ETLayoutItem+Factory.h"
+#import "ETUIItemFactory.h"
 #import "ETWindowItem.h"
 #import "NSWindow+Etoile.h"
+#import "ETCompatibility.h"
 
 #define DEFAULT_FRAME NSMakeRect(0, 0, 200, 200)
 
@@ -100,7 +71,8 @@
 when a layout other than ETWindowLayout is set on the receiver. */
 - (ETWindowItem *) createRootWindowItem
 {
-	return [ETLayoutItem itemWithWindow: AUTORELEASE([[ETFullScreenWindow alloc] init])];
+	return [[ETUIItemFactory factory] itemWithWindow: 
+		AUTORELEASE([[ETFullScreenWindow alloc] init])];
 }
 
 - (id) init
@@ -153,7 +125,7 @@ DEALLOC(DESTROY(_rootWindowItem); DESTROY(_visibleWindows));
 	// NOTE: We could eventually check whether the item to decorate already 
 	// has a window decorator before creating a new one that will be 
 	// refused by -setDecoratorItem: and hence never used. 
-	[[item lastDecoratorItem] setDecoratorItem: (ETDecoratorItem *)[ETWindowItem item]];
+	[[item lastDecoratorItem] setDecoratorItem: [ETWindowItem item]];
 	RELEASE(item);
 }
 
