etoile/EtoileUI

commit 55f79e1759cd685417e1bf10d27da613940ad3c3

Preliminary rewrite and simplification of the layout item visibility handling.
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5205 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETLayoutItem.m
@@ -968,12 +968,30 @@ The new selection state won't be apparent until a redisplay occurs. */
 	return _selected;
 }
 
+- (BOOL) canBecomeVisible
+{
+	return ([[self layout] isOpaque] == NO);
+}
+
 /** Sets whether the receiver should be displayed or not.
 
 The new visibility state won't be apparent until a redisplay occurs. */
 - (void) setVisible: (BOOL)visible
 {
 	_visible = visible;
+	if (visible)
+	{
+		if ([[[_parentItem supervisorView] subviews] containsObject: [self displayView]] == NO)
+		{
+			[_parentItem handleAttachViewOfItem: self];
+			ETDebugLog(@"Inserted view at %@", NSStringFromRect([self frame]));
+		}
+	}
+	else
+	{
+		[[self displayView] removeFromSuperview];
+		ETDebugLog(@"Removed view at %@", NSStringFromRect([self frame]));
+	}
 }
 
 /** Returns whether the receiver should be displayed or not. See also -setVisible:. */

Source/ETLayoutItemGroup.m
@@ -1284,29 +1284,11 @@ visible property of the child items. You shouldn't need to call this method by
 yourself (see -visibleItemsForItems:). */
 - (void) setVisibleItems: (NSArray *)visibleItems forItems: (NSArray *)items
 {
-	ETView *container = [self setUpSupervisorViewWithFrame: [self frame]];
+	[self setUpSupervisorViewWithFrame: [self frame]];
 
 	FOREACH(items, item, ETLayoutItem *)
 	{
-		if ([visibleItems containsObject: item])
-		{
-			[item setVisible: YES];
-			if (container != nil && [[container subviews] containsObject: [item displayView]] == NO
-			     && [item displayView] != nil )
-			{
-				[container addSubview: [item displayView]];
-				ETDebugLog(@"Inserted view at %@", NSStringFromRect([[item displayView] frame]));
-			}
-		}
-		else
-		{
-			[item setVisible: NO];
-			if (container != nil && [[container subviews] containsObject: [item displayView]])
-			{
-				[[item displayView] removeFromSuperview];
-				ETDebugLog(@"Removed view at %@", NSStringFromRect([[item displayView] frame]));
-			}
-		}
+		[item setVisible: [visibleItems containsObject: item]];
 	}
 }
 

commit 47a1cae9548d0475c040206079138a4587088864

Fixed issues introduced with r5205. e.g. blank content when the window layer is switched to a layout such as ETFreeLayout.
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5206 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETLayer.m
@@ -103,11 +103,19 @@ when a layout other than ETWindowLayout is set on the receiver. */
 	// the item, the display view is the window view (NSThemeFrame on Mac OS X)
 	// Removing NSThemeFrame results in a very weird behavior: the window 
 	// remains visible but a -lockFocus assertion is thrown on mouse down.
+	if ([[self layout] isKindOfClass: [ETWindowLayout class]])
+		return;
+
+	[super handleAttachViewOfItem: item];
 }
 
 - (void) handleDetachViewOfItem: (ETLayoutItem *)item
 {
 	// Ditto. More explanations in -handleDetachItem:.
+	if ([[self layout] isKindOfClass: [ETWindowLayout class]])
+		return;
+
+	[super handleDetachViewOfItem: item];
 }
 
 - (void) handleAttachItem: (ETLayoutItem *)item

Source/ETLayoutItem.m
@@ -989,7 +989,7 @@ The new visibility state won't be apparent until a redisplay occurs. */
 	}
 	else
 	{
-		[[self displayView] removeFromSuperview];
+		[_parentItem handleDetachViewOfItem: self];
 		ETDebugLog(@"Removed view at %@", NSStringFromRect([self frame]));
 	}
 }