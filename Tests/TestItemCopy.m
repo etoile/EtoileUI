1d0d103ea6db6cfebbb587b90b80ad43aa9e283e
Quentin Mathé <quentin.mathe@gmail.com>
2009-08-30 21:37:50 +0000

    Forgot to commit the new tests in the previous commit
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5243 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/TestItemCopy.m
+++ Tests/TestItemCopy.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 #import <UnitKit/UnitKit.h>
+#import <EtoileFoundation/ETUTI.h>
 #import <EtoileFoundation/Macros.h>
 #import <EtoileFoundation/NSObject+Model.h>
 #import "ETDecoratorItem.h"
@@ -43,13 +44,44 @@
 
 DEALLOC(DESTROY(itemFactory); DESTROY(item))
 
+- (NSArray *) nonCheckablePropertiesForAnyObject
+{
+	NSArray *rootObjectProperties = [(NSObject *)AUTORELEASE([[NSObject alloc] init]) properties];
+	return [rootObjectProperties arrayByRemovingObjectsInArray:
+		A(kETNameProperty, kETDisplayNameProperty, kETIconProperty)];
+}
+
+- (NSArray *) checkablePropertiesForItem: (ETUIItem *)anItem
+{
+	NSArray *excludedProperties = [self nonCheckablePropertiesForAnyObject];
+	return [[anItem properties] arrayByRemovingObjectsInArray: excludedProperties];
+}
+
+- (void) checkViewCopy: (ETView *)newView ofView: (ETView *)aView
+{
+	NSArray *equalProperties = A(@"x", @"y", @"width", @"height", @"tag", 
+		@"hidden", @"autoresizingMask", @"autoresizesSubviews", @"flipped", 
+		@"frame", @"frameRotation", @"bounds", @"boundsRotation", 
+		@"isRotatedFromBase", @"isRotatedOrScaledFromBase", 
+		@"postsFrameChangedNotifications", @"postsBoundsChangedNotifications", 
+		@"visibleRect", @"opaque", @"needsPanelToBecomeKey"); 
+
+	FOREACH(equalProperties, property, NSString *)
+	{
+		/* We could use -valueForKey: as well */
+		id value = [aView valueForProperty: property];
+		id copiedValue = [newView valueForProperty: property];
+		if ([value isEqual: copiedValue] == NO)
+		{
+			NSLog(@"Fail!!!");
+		}
+		UKObjectsEqual(value, copiedValue);
+	}
+}
+
 - (void) testBasicItemCopy
 {
-	ETLayoutItem *newItem = [item copy];
-	NSArray *rootObjectProperties = [(NSObject *)AUTORELEASE([[NSObject alloc] init]) properties];
-	NSArray *excludedProperties = [rootObjectProperties arrayByRemovingObjectsInArray:
-		A(kETNameProperty, kETDisplayNameProperty, kETIconProperty)];
-	NSArray *properties = [[item properties] arrayByRemovingObjectsInArray: excludedProperties];
+	NSArray *properties = [self checkablePropertiesForItem: item];
 	NSArray *nilProperties = A(kETNameProperty, kETIconProperty, kETImageProperty,
 		kETRepresentedObjectProperty, kETRepresentedPathBaseProperty, 
 		kETSubtypeProperty, kETActionProperty, kETTargetProperty);
@@ -60,8 +92,11 @@ DEALLOC(DESTROY(itemFactory); DESTROY(item))
 	NSArray *equalProperties = [properties arrayByRemovingObjectsInArray: 
 		[nonEqualProperties arrayByAddingObjectsFromArray: nilProperties]];
 
+	ETLayoutItem *newItem = [item copy];
+
 	FOREACH(equalProperties, property, NSString *)
 	{
+		/* We could use -valueForKey: as well, see -testItemCopy */
 		id value = [item valueForProperty: property];
 		id copiedValue = [newItem valueForProperty: property];
 
@@ -78,7 +113,51 @@ DEALLOC(DESTROY(itemFactory); DESTROY(item))
 
 - (void) testItemCopy
 {
+	[item setName: @"Whatever"];
+	[item setImage: [NSImage imageNamed: @"NSApplicationIcon"]];
+	[item setIcon: [[NSWorkspace sharedWorkspace] iconForFile: @"/"]];
+	[item setRepresentedObject: [NSSet set]];
+	//[item setRepresentedPathBase: @"/my/model/path"];
+	[item setSubtype: [ETUTI typeWithClass: [NSSet class]]];
+	//[item setController: AUTORELEASE([[ETController alloc] init])];
+	[item setTarget: self];
+	[item setAction: @selector(wibble:)];
+	[item setView: AUTORELEASE([[NSButton alloc] init])];
 
+	NSArray *properties = [self checkablePropertiesForItem: item];
+	NSArray *nilProperties = A(kETRepresentedPathBaseProperty);
+	NSArray *nonEqualProperties = A(kETDisplayNameProperty, 
+		kETDecoratorItemProperty, kETDecoratedItemProperty, @"firstDecoratedItem", 
+		@"lastDecoratorItem", @"enclosingItem", @"supervisorView", kETViewProperty,
+		kETParentItemProperty, kETStyleGroupProperty, kETLayoutProperty);
+	NSArray *equalProperties = [properties arrayByRemovingObjectsInArray: 
+		[nonEqualProperties arrayByAddingObjectsFromArray: nilProperties]];
+
+	ETLayoutItem *newItem = [item copy];
+
+	FOREACH(equalProperties, property, NSString *)
+	{
+		/* We don't want to check the properties on the represented object but 
+		   on the item itself, so we must use -valueForKey: which has no custom 
+		   lookup policy on the represented object unlike -valueForProperty:. */
+		id value = [item valueForKey: property];
+		id copiedValue = [newItem valueForKey: property];
+
+		if ([value isEqual: copiedValue] == NO)
+		{
+			NSLog(@"Fail!!!");
+		}
+		UKObjectsEqual(value, copiedValue);
+	}
+
+	UKNil([newItem decoratorItem]);
+	UKNil([newItem decoratedItem]);
+	UKObjectsEqual([[newItem supervisorView] class], [[item supervisorView] class]);
+	UKNil([newItem parentItem]);
+	// TODO: Set a layout and UKNotNil([newItem layout]);
+	UKObjectsEqual([[newItem view] class], [[item view] class]);
+
+	[self checkViewCopy: [newItem supervisorView] ofView: [item supervisorView]];
 }
 
 - (void) testEmptyItemGroupCopy
