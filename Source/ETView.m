etoile/EtoileUI

commit da68c7c4566f9083dce3765f3e92956073a4cbc2

Cleaned -setContentView:temporary: a bit and improved view validity checks.
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5262 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETView.m
@@ -403,26 +403,63 @@ installed by the layout. */
 	return _temporaryView;
 }
 
+- (void) checkViewHierarchyValidity
+{
+	if (_wrappedView != nil)
+	{
+		NSAssert1([[_wrappedView superview] isEqual: self], @"The wrapped view"
+			"%@ has been wrongly moved to a superview without first removing "
+			"it explicitely with -[ETLayoutItem setView: nil] or "
+			"-[ETView setWrappedView: nil]", _wrappedView);
+	}
+	if (_temporaryView != nil)
+	{
+		NSAssert1([[_temporaryView superview] isEqual: self], @"The temporary view"
+			"%@ has been wrongly moved to a superview without first removing "
+			"it explicitely with -[ETLayoutItem setLayoutView: nil] or "
+			"-[ETView setTemporaryView: nil]", _temporaryView);
+	}
+}
+
+/* Not really costly since aView is expected to have no superview normally. */
+- (BOOL) hasSupervisorViewAncestor: (NSView *)aView
+{
+	NSView *ancestorView = [aView superview];
+
+	while (nil != ancestorView)
+	{
+		if ([ancestorView isSupervisorView])
+			return YES;
+
+		ancestorView = [ancestorView superview];
+	}
+
+	return NO;
+}
+
 /** This method is only exposed to be used internally by EtoileUI. 
 
 You must override this method in subclasses. */
 - (void) setContentView: (NSView *)view temporary: (BOOL)temporary
 {
+	[self checkViewHierarchyValidity];
+
+	if (view != nil && [[self layoutItem] isDecoratorItem] == NO)
+	{
+		NSString *selSubstring = (temporary ? @"Temporary" : @"Wrapped");
+
+		NSAssert2([self hasSupervisorViewAncestor: view] == NO, @"You must not move "
+			"view %@ to a new superview without first removing it explicitely "
+			"with -[ETLayoutItem setView: nil] or -[ETView set%@View: nil]", 
+			view, selSubstring);
+	}
+
 	/* Reset the content view frame to fill the receiver */
 	[view setFrameOrigin: NSZeroPoint];
 	[view setFrameSize: [self frame].size];
 
 	/* Ensure the resizing of all subviews is handled automatically */
 	[self setAutoresizesSubviews: YES];
-
-	if ([self wrappedView] != nil)
-	{
-		NSAssert1([[self wrappedView] isDescendantOf: self], @"You must not "
-			"move view %@ to a superview without first removing it explicitely "
-			"with -[ETLayoutItem setView: nil] or -[ETView setWrappedView: nil]", 
-			[self wrappedView]);
-		[[self wrappedView] removeFromSuperview];
-	}
 
 	if (temporary) /* Temporary view setter */
 	{
@@ -451,6 +488,7 @@ You must override this method in subclasses. */
 		{
 			/* Restore autoresizing mask */
 			//[[self wrappedView] setAutoresizingMask: [self autoresizingMask]];
+			[[self wrappedView] removeFromSuperview];
 		}
 	}
 }

commit 08ca133d5c6f16f877e8b53f7646f3040160be66

Fixed missing wrapped view removal when a new one is inserted. This bug was introduced with r5262 and was visible in the icon layout where every icon was displayed twice (two views overlapped).
git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5378 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

Source/ETView.m
@@ -360,6 +360,9 @@ Recomputes the positioning of the main view. */
 /** Sets the item view.<br />
 The receiver is the view owner.
 
+If a temporary view is currently set, the new wrapped view won't become visible 
+until you remove the temporary view.
+
 You must never call this method but -[ETLayoutItem setView:]. */
 - (void) setWrappedView: (NSView *)view
 {
@@ -442,7 +445,10 @@ installed by the layout. */
 
 /** This method is only exposed to be used internally by EtoileUI. 
 
-You must override this method in subclasses. */
+You must override this method in subclasses. 
+
+See -setWrappedView: and -setTemporaryView: documentation which explains the 
+implemented behavior. */
 - (void) setContentView: (NSView *)view temporary: (BOOL)temporary
 {
 	[self checkViewHierarchyValidity];
@@ -466,6 +472,12 @@ You must override this method in subclasses. */
 
 	if (temporary) /* Temporary view setter */
 	{
+		NSParameterAssert(_wrappedView == nil || [_wrappedView superview] == self);
+
+		/* In case a temporary view is already in use, we remove it */
+		[[self temporaryView] removeFromSuperview];
+
+
 		if (view != nil)
 		{
 			[view setAutoresizingMask: NSViewWidthSizable | NSViewHeightSizable];
@@ -476,12 +488,17 @@ You must override this method in subclasses. */
 		{
 			/* Restore autoresizing mask */
 			//[[self temporaryView] setAutoresizingMask: [self autoresizingMask]];
-			[[self temporaryView] removeFromSuperview];
+
 			[[self wrappedView] setHidden: NO];
 		}
 	}
 	else /* Wrapped view setter */
 	{
+		NSParameterAssert(nil == _temporaryView);
+
+		/* In case a wrapped view is already in use, we remove it */
+		[[self wrappedView] removeFromSuperview];
+
 		if (view != nil)
 		{
 			[view setAutoresizingMask: NSViewWidthSizable | NSViewHeightSizable];
@@ -491,7 +508,6 @@ You must override this method in subclasses. */
 		{
 			/* Restore autoresizing mask */
 			//[[self wrappedView] setAutoresizingMask: [self autoresizingMask]];
-			[[self wrappedView] removeFromSuperview];
 		}
 	}
 }