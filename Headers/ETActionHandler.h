5911821d0d771026b360f711c3584373317a6a2a
Quentin Mathé <quentin.mathe@gmail.com>
2009-11-21 11:49:42 +0000

    Removed outdated comments and TODOs
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5580 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/ETActionHandler.h
+++ Headers/ETActionHandler.h
@@ -13,11 +13,7 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
-/* WARNING: Unstable API, the method names won't radically change but the event 
-            handler concept will be refined as pluggable aspect and reworked to 
-            support the notion of instruments that can encapsulate the logic of
-            graphic tools for example. 
-            More explanations available in TODO file. */
+// WARNING: Unstable API.
 
 @class ETUTI;
 @class ETEvent, ETLayoutItem, ETPickboard, ETPickDropCoordinator;
@@ -112,79 +108,3 @@ method will reuse the action handler to be set on every created items. */
 + (Class) styleClass;
 
 @end
-
-/** Event Handling in the Layout Item Tree 
-
-	What follows is only valid when the layout item tree is rendered by 
-	EtoileUI/AppKit rendering backend (see ETEtoileUIRender).
-	The events are routed through the AppKit view hierarchy to the proper child
-	item of the layout item matching the view that replies YES to -hitTest. If 
-	the view has no corresponding layout item and returns YES, the event 
-	handling remains on AppKit side (following AppKit event model). If this 
-	view returns NO, then -hitTest is done on the superview which may have a 
-	matching layout item or not. 
-	ETView class and subclasses replies YES to -hitTest unlike NSView, so the
-	event they received are always routed to their corresponding layout item.
-	Basic event handling methods in ETLayoutItem reuses the name of their 
-	equivalent NSResponder methods plus an additional method keywoard 'on:'.
-	Here is an example:
-	- (void) mouseDown: (NSEvent *)event in Responder
-	becomes
-	- (void) mouseDown: (ETEvent *)event on: (id)item in ETLayoutItem.
-	Presently these methods are declared on ETLayoutItem, although in future
-	they will be moved to a separate ETEventHandler class making possible to
-	to changing event handling at runtime by plugging another event handler.
-	This will help to support multiple interaction modes very easily as needed 
-	by many graphics applications and to implement custom mouse tracking 
-	(radio buttons, menus, custom controls etc.) without subclassing.
-	
-	When a layout item receives an event, it looks up for the higher ancestor 
-	item that wants to preempt the event. For example, if the root item has an
-	outline layout active instead of the common UI layout, then the root item
-	will handles the event in all cases. Even if the item which receives the
-	event is ten levels below or more. This rule is important because it allows
-	transparent pick and drop: if the item receiving the event has a parent 
-	which uses a table layout, a drag should be handled according to the drag 
-	semantic defined by table layout, but this doesn't hold if this table
-	layout is invisible because some higher ancestor item in the tree uses an
-	outline layout at this time.
-	Most of time there are no ancestor items which want to preempt the event, 
-	in this case
-	
-	Here is a list of layouts that preempts events:
-	- ETTableLayout
-	- ETOutlineLayout
-	- ETBrowserLayout
-	- ETModelViewLayout
-	- any layouts which uses a layout view (injected in a container when you 
-	  call -setLayout: on its layout item).
-	  
-	Layouts which don't preempt events:
-	- ETFlowLayout
-	- ETColumnLayout
-	- ETLineLayout
-	- ETFreeLayout
-	- ETUILayout
-	
-	If you use a source with your container and you implement mutation source 
-	methods, you don't need to worry about pick and drop interaction handling.
-	ETLayoutItemGroup mutation methods like -addItem:, -removeItem: etc. hides 
-	all the details involved by dealing with a source when one is set. These 
-	methods takes care of finding whether the item is provided by a source or 
-	not by looking up in the layout item tree.
-	If an item group has a source which doesn't implement mutation methods. The
-	selected and related children items will use another selection color (or
-	visual indicator) than the usual one. This makes clear whether a selected 
-	item will be picked if you try to. Usually unless the user tweaks the UI, he
-	usually won't see two selection colors in a table to take an example. 
-	(see ETLayoutItemGroup which provides detailed explanations about related 
-	items and how a source can be overriden in a layout item subtree). 
-	
-	- (NSArray *)namesOfPromisedFilesDroppedAtDestination:(NSURL *)dropDestination 
-	isn't officially supported by EtoileUI, all other methods of NSDraggingSource
-	are implemented in ETGroupEventHandler and can be overriden. 
-	WARNING: ETGroupEventHandler doesn't exist yet and is currently part of 
-	ETLayoutItem (Events). */
-
-// TODO: Refactor this category in a pluggable event handler object. 
-// A new ETInteraction class or class hierachy should be introduced.
