2a7af738489d172c915db927a399e990bcde8c12
Quentin Mathé <quentin.mathe@gmail.com>
2009-11-27 16:08:45 +0000

    Fixed Mac OS X KVO crash on -objectValue change by checking the recursion level as GNUstep does (e.g. to click a text field was resulting in a crash).
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@5591 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/Controls+Etoile.m
+++ Source/Controls+Etoile.m
@@ -140,3 +140,71 @@ terminology) on which actions should be dispatched. */
 
 @end
 
+#ifndef GNUSTEP
+
+// NOTE: We might need to make the same changes to NSControl.
+@interface NSCell (Etoile)
+- (void) willChangeValueForKey: (NSString *)aKey;
+- (void) didChangeValueForKey: (NSString *)aKey;
+@end
+
+@implementation NSCell (Etoile)
+
+/* We override this method to make KVO resilient to some recursion issues on 
+Mac OS X (GNUstep KVO handles this transparently in a way similar to what we implement)
+Such a recursion occurs when -didChangeValueForKey: is invoked by the getter 
+that corresponds to this key.
+e.g. on Mac OS X, we have roughly:
+8 ...
+7 -didChangeValueForKey:
+6 -validateEditing
+5 -[NSCell stringValue]
+4 -valueForKey:
+3 -didChangeValueForKey:
+2 -validateEditing
+1 -[NSCell stringValue]
+
+This workaround implies that the getters/setters such as -objectValue and 
+-setObjectValue: have to be reentrant. It seems to be the case on GNUstep and 
+probably on Cocoa but there is no way to be sure with the latter.
+
+The KVO workaround below is not thread-safe, but the AppKit is generally not, so 
+it shouldn't be a problem with NSControl/NSCell class hierarchy.
+
+KVO is thread-safe, except that willChangeXXX/change/didChangeXXX sequence 
+is never atomic in either automatic and manual KVO. 
+Two threads can enters the same setter simultaneously, then two willChangeXXX 
+messages might immediately follow each other unlike what you expect.
+See also http://lists.apple.com/archives/cocoa-dev/2007/May/msg00022.html */
+
+static NSString *willChangeUnderway = nil;
+
+- (void) willChangeValueForKey: (NSString *)aKey
+{
+	if (nil != willChangeUnderway && [willChangeUnderway isEqualToString: aKey])
+		return;
+
+	willChangeUnderway = aKey;
+
+	[super willChangeValueForKey: aKey];
+
+	willChangeUnderway = nil;
+}
+
+static NSString *didChangeUnderway = nil;
+
+- (void) didChangeValueForKey: (NSString *)aKey
+{
+	if (nil != didChangeUnderway && [didChangeUnderway isEqualToString: aKey])
+		return;
+
+	didChangeUnderway = aKey;
+
+	[super didChangeValueForKey: aKey];
+
+	didChangeUnderway = nil;
+}
+
+@end
+
+#endif
