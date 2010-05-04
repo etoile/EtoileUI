dadb85cb23b2d02f27cd86a2e947e23f0588703c
Quentin Mathé <quentin.mathe@gmail.com>
2010-05-04 20:31:43 +0000

    Corrected EtoileUI header imports to be local
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@6081 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/test_ETLayoutItemBuilder.m
+++ Tests/test_ETLayoutItemBuilder.m
@@ -1,45 +1,20 @@
 /*
-	test_ETLayoutItemBuilder.m
-
 	Copyright (C) 2007 Quentin Mathe
 
-	Author:  Quentin Mathe <qmathe@club-internet.fr>
+	Author:  Quentin Mathe <quentin.mathe@gmail.com>
 	Date:  November 2007
-
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License:  Modified BSD (see COPYING)
  */
 
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
-#import <EtoileUI/ETLayoutItemBuilder.h>
-#import <EtoileUI/ETLayoutItem.h>
-#import <EtoileUI/ETLayoutItem+Factory.h>
-#import <EtoileUI/ETView.h>
-#import <EtoileUI/ETContainer.h>
-#import <EtoileUI/ETCompatibility.h>
 #import <UnitKit/UnitKit.h>
+#import "ETLayoutItemBuilder.h"
+#import "ETLayoutItem.h"
+#import "ETLayoutItem+Factory.h"
+#import "ETView.h"
+#import "ETContainer.h"
+#import "ETCompatibility.h"
 
 @interface ETEtoileUIBuilder (UnitKitTests) <UKTest>
 @end
@@ -55,32 +30,6 @@
 - (void) testRender
 {
 
-}
-
-
-- (void) testRenderWindow
-{
-	int styleMask = NSTitledWindowMask | NSClosableWindowMask 
-		| NSMiniaturizableWindowMask | NSResizableWindowMask;
-	id window = [[NSWindow alloc] initWithContentRect: NSMakeRect(100, 200, 400, 300) 
-	                                        styleMask: styleMask 
-											  backing: NSBackingStoreBuffered
-											    defer: NO 
-											   screen: nil];
-
-	[window setTitle: @"My Window Title"];
-	
-#if 0	
-	id item = [[ETEtoileUIBuilder builder] renderWindow: window];
-	
-	UKNotNil(item);
-	UKObjectsSame(window, [[item lastDecoratorItem] representedObject]);
-	UKTrue(NSEqualRects([window frame], [item frame]));
-	UKObjectsEqual([window title], [item name]);
-	UKObjectsSame([window contentView], [item displayView]);
-#endif
-
-	RELEASE(window);
 }
 
 - (void) testRenderView
