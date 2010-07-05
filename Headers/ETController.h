85a5028addf291c155fd7a99df1c9191d2f4e924
Quentin Mathé <quentin.mathe@gmail.com>
2010-07-05 10:12:46 +0000

    Reformatted ETController description doc
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@6211 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/ETController.h
+++ Headers/ETController.h
@@ -18,41 +18,45 @@
 // TODO: Think about the selection marker stuff and implement it if it makes senses.
 
 /** ETController provides a generic controller layer, usually to be used a 
-	replacement for both NSArrayController and NSTreeController. This reusable 
-	controller fills the same purpose than the previously mentionned 
-	NSController subclasses. The difference lies in the fact the tree structure 
-	is already abstracted in the layout item tree, as such there is no need for 
-	a special tree controller class with EtoileUI. The mediation with a 
-	collection of model objects is also abstracted in ETLayoutItemGroup class, 
-	whose instances play the role of lightweight mediators between View and 
-	Model in MVC term, hence NSArrayController isn't really needed either.
-	ETController extends the traditional facilities of NSController subclasses 
-	by allowing to make a distinction between Object class and Group class 
-	(leaf vs branches) as very often needed by applications in the Object 
-	Manager style (see also CoreObject) at both UI and model levels. 
-	For the UI, you can specify items templates to be cloned when a new element 
-	has to be inserted/added. On the model side, you can specify the class of 
-	the model objects to be instantiated.
+replacement for both NSArrayController and NSTreeController. 
 
-	be exactly accurate or entirely implemented.
-	This whole facility can be used at any levels of the UI. For example, for  
-	supporting multiple windows in a file manager, you can create an 
-	ETLayoutItemGroup instance, that encapsulates a layout item tree which is 
-	the file manager UI. Then by simply setting this item group as an item group 
-	template on the window layer, and wiring File->New Window... to -add: 
-	action of this controller, new file manager windows will be created when 
-	New Window... is clicked in the menu. If you want your file managers to 
-	automatically open on a default directory, a model class can be set on the 
-	on the controller (-setGroupClass:) or alternatively a model instance can be 
-	set on the template item group (by calling -[ETLayoutItem setRepresentedObject:]).
-	This ability to clone entire UI is detailed in -deepCopy of ETLayoutItem 
-	class and subclasses.
-	When a controller is set, it's very important to ensure its layout item is 
-	a base item, otherwise the wrong template items may be look up in the 
-	ancestor layout item instead of using the one declared in the controller 
-	bound to the container. 
-	ETController directly sorts object of the content and doesn't maintain 
-	arranged objects as a collection distinct from the content. */
+This reusable controller fills the same purpose than the previously mentionned 
+NSController subclasses. The difference lies in the fact the tree structure is 
+already abstracted in the layout item tree, as such there is no need for a 
+special tree controller class with EtoileUI. The mediation with a collection of 
+model objects is also abstracted in ETLayoutItemGroup class, whose instances 
+play the role of lightweight mediators between View and Model in MVC term, hence 
+NSArrayController isn't really needed either.
+
+ETController extends the traditional facilities of NSController subclasses 
+by allowing to make a distinction between Object class and Group class 
+(leaf vs branches) as very often needed by applications in the Object Manager 
+style (see also CoreObject) at both UI and model levels.</ br>
+For the UI, you can specify items templates to be cloned when a new element 
+has to be inserted/added. On the model side, you can specify the class of 
+the model objects to be instantiated.
+
+This whole facility can be used at any levels of the UI.<br />
+For example, for  supporting multiple windows in a file manager, you can create 
+an ETLayoutItemGroup instance, that encapsulates a layout item tree which is the 
+file manager UI. Then by simply setting this item group as an item group 
+template on the window layer, and wiring <em>File->New Window...</em> to -add: 
+action of this controller, new file manager windows will be created when 
+<em>New Window...</em>is clicked in the menu.<br />
+If you want your file managers to automatically open on a default directory, a 
+model class can be set on the on the controller (-setGroupClass:) or 
+alternatively a model instance can be set on the template item group (by 
+calling -[ETLayoutItem setRepresentedObject:]). 
+
+That ability to clone entire UI is detailed in -deepCopy of ETLayoutItem class 
+and subclasses.
+
+When a controller is set, it's very important to ensure its layout item is a 
+base item, otherwise the wrong template items may be look up in the ancestor 
+layout item instead of using the one declared in the controller bound to the container. 
+
+ETController directly sorts object of the content and doesn't maintain arranged 
+objects as a collection distinct from the content. */
 @interface ETController : ETNibOwner <NSCopying>
 {
 	@private
