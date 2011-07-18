a696723129bc6a77b6434c8937f48d8accc95f59
Quentin Mathé <quentin.mathe@gmail.com>
2011-07-18 10:47:51 +0000

    Added ETActionHandler model description
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@7108 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ ModelDescription/ETActionHandler+ModelDescription.m
@@ -0,0 +1,34 @@
+/*
+	Copyright (C) 2011 Quentin Mathe
+
+	Author:  Quentin Mathe <quentin.mathe@gmail.com>
+	Date:  June 2011
+	License: Modified BSD (see COPYING)
+ */
+
+#import <EtoileFoundation/EtoileFoundation.h>
+#import "ETActionHandler.h"
+
+@interface ETActionHandler (ModelDescription)
+@end
+
+@implementation ETActionHandler (ModelDescription)
+
++ (ETEntityDescription *) newEntityDescription
+{
+	ETEntityDescription *entity = [self newBasicEntityDescription];
+
+	// For subclasses that don't override -newEntityDescription, we must not add 
+	// the property descriptions that we will inherit through the parent
+	if ([[entity name] isEqual: [ETActionHandler className]] == NO) 
+		return entity;
+
+	ETPropertyDescription *fieldEditorItem = 
+		[ETPropertyDescription descriptionWithName: @"fieldEditorItem" type: (id)@"ETLayoutItem"];
+
+	[entity setPropertyDescriptions: A(fieldEditorItem)];
+
+	return entity;
+}
+
+@end
--- ModelDescription/ETLayoutItem+ModelDescription.m
+++ ModelDescription/ETLayoutItem+ModelDescription.m
@@ -32,6 +32,7 @@
 	ETPropertyDescription *repObject = [ETPropertyDescription descriptionWithName: @"representedObject" type: (id)@"NSObject"];
 	ETPropertyDescription *styleGroup = [ETPropertyDescription descriptionWithName: @"styleGroup" type: (id)@"ETStyleGroup"];
 	ETPropertyDescription *coverStyle = [ETPropertyDescription descriptionWithName: @"coverStyle" type: (id)@"ETStyle"];
+	ETPropertyDescription *actionHandler = [ETPropertyDescription descriptionWithName: @"actionHandler" type: (id)@"ETActionHandler"];
 	ETPropertyDescription *contentBounds = [ETPropertyDescription descriptionWithName: @"contentBounds" type: (id)@"NSRect"];
 	ETPropertyDescription *position = [ETPropertyDescription descriptionWithName: @"position" type: (id)@"NSPoint"];
 	ETPropertyDescription *anchorPoint = [ETPropertyDescription descriptionWithName: @"anchorPoint" type: (id)@"NSPoint"];
@@ -54,7 +55,7 @@
 	   Hmm, _scrollViewShow ought to be persisted. */
 
 	NSArray *persistentProperties = A(parentItem, repObject, styleGroup, 
-		coverStyle, contentBounds, position, anchorPoint, autoresizing, 
+		coverStyle, actionHandler, contentBounds, position, anchorPoint, autoresizing, 
 		contentAspect, boundingBox, flipped, selected, visible);
 
 	[[persistentProperties mappedCollection] setPersistent: YES];
