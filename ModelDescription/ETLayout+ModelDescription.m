6b607d1a536cb043a1d8b650f11e9e56377d155e
Quentin Mathé <quentin.mathe@gmail.com>
2011-07-18 10:53:10 +0000

    Filled ETLayout model description
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@7110 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- ModelDescription/ETLayout+ModelDescription.m
+++ ModelDescription/ETLayout+ModelDescription.m
@@ -27,13 +27,48 @@
 	ETPropertyDescription *context = 
 		[ETPropertyDescription descriptionWithName: @"layoutContext" type: (id)@"ETLayoutItemGroup"];
 	[context setOpposite: (id)@"ETLayoutItemGroup.layout"];
+	ETPropertyDescription *delegate = 
+		[ETPropertyDescription descriptionWithName: @"delegate" type: (id)@"NSObject"];
+	ETPropertyDescription *layoutView = 
+		[ETPropertyDescription descriptionWithName: @"layoutView" type: (id)@"NSView"];
+	ETPropertyDescription *tool = 
+		[ETPropertyDescription descriptionWithName: @"tool" type: (id)@"ETTool"];
+	ETPropertyDescription *layerItem = 
+		[ETPropertyDescription descriptionWithName: @"layerItem" type: (id)@"ETLayoutItemGroup"];
+	ETPropertyDescription *dropIndicator = 
+		[ETPropertyDescription descriptionWithName: @"dropIndicator" type: (id)@"ETDropIndicator"];
+
+	// NOTE: layoutSize is not transient, it is usually computed but can be customized
+	ETPropertyDescription *layoutSize = 
+		[ETPropertyDescription descriptionWithName: @"layoutSize" type: (id)@"NSSize"];
+	ETPropertyDescription *isContentSizeLayout = 
+		[ETPropertyDescription descriptionWithName: @"isContentSizeLayout" type: (id)@"BOOL"];
+	ETPropertyDescription *layoutSizeCustomized = 
+		[ETPropertyDescription descriptionWithName: @"layoutSizeCustomized" type: (id)@"BOOL"];	
+	ETPropertyDescription *itemSize = 
+		[ETPropertyDescription descriptionWithName: @"itemSize" type: (id)@"NSSize"];	
+	ETPropertyDescription *itemSizeConstraintStyle = 
+		[ETPropertyDescription descriptionWithName: @"itemSize" type: (id)@"NSUInteger"];	
+
+	// TODO: Declare the numerous derived (implicitly transient) properties we have 
 
 	/* Transient properties
-	   _layoutContext (weak ref) */
+	   _tool, _dropIndicator, _isLayouting */
+	NSArray *transientProperties = A(tool, dropIndicator, layerItem);
 
-	[entity setPropertyDescriptions: A(context)];
+	// TODO: Support tool persistence... Rarely needed though.
+	// TODO: We need a direct ivar access to persist the layer item
+	// TODO: Evaluate whether we should support drop indicator persistence
+	NSArray *persistentProperties = A(context, delegate, layoutView,  
+		layoutSize, isContentSizeLayout, layoutSizeCustomized, itemSize, itemSizeConstraintStyle);
+
+	[[persistentProperties mappedCollection] setPersistent: YES];
+	[entity setPropertyDescriptions: 
+		[persistentProperties arrayByAddingObjectsFromArray: transientProperties]];
 
 	return entity;
 }
 
 @end
+
+// NOTE: ETFixedLayout uses ETLayout model description
