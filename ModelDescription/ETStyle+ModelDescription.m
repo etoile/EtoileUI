73feb035796eb0a4f871c3565dc0364a3d0095d9
Quentin Mathé <quentin.mathe@gmail.com>
2013-05-20 16:37:51 +0000

    Improved ETStyle+ModelDescription for UI Builder
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@8344 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- ModelDescription/ETStyle+ModelDescription.m
+++ ModelDescription/ETStyle+ModelDescription.m
@@ -7,6 +7,7 @@
  */
 
 #import <EtoileFoundation/EtoileFoundation.h>
+#import "ETBasicItemStyle.h"
 #import "ETStyle.h"
 #import "ETStyleGroup.h"
 #import "ETShape.h"
@@ -26,6 +27,8 @@
 		return entity;
 
 	ETPropertyDescription *isShared = [ETPropertyDescription descriptionWithName: @"isShared" type: (id)@"BOOL"];
+
+	[entity setUIBuilderPropertyNames: A([isShared name])];
 
 	[entity addPropertyDescription: isShared];
 
@@ -54,8 +57,9 @@
 	ETPropertyDescription *firstStyle = [ETPropertyDescription descriptionWithName: @"firstStyle" type: (id)@"ETStyle"];
 	ETPropertyDescription *lastStyle = [ETPropertyDescription descriptionWithName: @"lastStyle" type: (id)@"ETStyle"];
 
-	[styles setPersistent: YES];
+	[entity setUIBuilderPropertyNames:  A([styles name])];
 
+	[styles setPersistent: YES];
 	[entity setPropertyDescriptions: A(styles, firstStyle, lastStyle)];
 
 	return entity;
@@ -63,6 +67,57 @@
 
 @end
 
+@interface ETBasicItemStyle (ModelDescrition)
+@end
+
+@implementation ETBasicItemStyle (ModelDescription)
+
++ (ETEntityDescription *) newEntityDescription
+{
+	ETEntityDescription *entity = [self newBasicEntityDescription];
+	
+	// For subclasses that don't override -newEntityDescription, we must not add
+	// the property descriptions that we will inherit through the parent.
+	if ([[entity name] isEqual: [ETBasicItemStyle className]] == NO)
+		return entity;
+
+	// TODO: Add labelAttributes and selectedLabelAttributes
+	ETPropertyDescription *labelPosition = [ETPropertyDescription descriptionWithName: @"labelPosition" type: (id)@"NSUInteger"];
+	[labelPosition setRole: AUTORELEASE([ETMultiOptionsRole new])];
+	[[labelPosition role] setAllowedOptions:
+	 	[D(@(ETLabelPositionNone), _(@"None"),
+		   @(ETLabelPositionContentAspect), _(@"Based on Item Content Aspect"),
+		   @(ETLabelPositionCentered), _(@"Centered"),
+		   @(ETLabelPositionInsideLeft), _(@"Inside Left"),
+		   @(ETLabelPositionOutsideLeft), _(@"Outside Left"),
+		   @(ETLabelPositionInsideTop), _(@"Inside Top"),
+		   @(ETLabelPositionOutsideTop), _(@"Outside Top"),
+		   @(ETLabelPositionInsideRight), _(@"Inside Right"),
+		   @(ETLabelPositionOutsideRight), _(@"Outside Right"),
+		   @(ETLabelPositionInsideBottom), _(@"Inside Bottom"),
+		   @(ETLabelPositionOutsideBottom), _(@"Outside Bottom")) arrayRepresentation]];
+	ETPropertyDescription *labelMargin = [ETPropertyDescription descriptionWithName: @"labelMargin" type: (id)@"float"];
+	ETPropertyDescription *maxLabelSize = [ETPropertyDescription descriptionWithName: @"maxLabelSize" type: (id)@"NSSize"];
+	ETPropertyDescription *maxImageSize = [ETPropertyDescription descriptionWithName: @"maxImageSize" type: (id)@"NSSize"];
+	ETPropertyDescription *edgeInset = [ETPropertyDescription descriptionWithName: @"edgeInset" type: (id)@"float"];
+
+	NSArray *transientProperties = [NSArray array];
+	NSArray *persistentProperties = A(labelPosition, labelMargin, maxLabelSize,
+		maxImageSize, edgeInset);
+
+	[entity setUIBuilderPropertyNames: (id)[[A(edgeInset, labelPosition,
+		labelMargin, maxLabelSize, maxImageSize) mappedCollection] name]];
+
+	// TODO: Turn on once the persistency test suite is updated
+	//[[persistentProperties mappedCollection] setPersistent: YES];
+	[entity setPropertyDescriptions:
+		[persistentProperties arrayByAddingObjectsFromArray: transientProperties]];
+
+	
+	return entity;
+}
+
+@end
 
 @interface ETShape (ModelDescrition)
 @end
@@ -88,6 +143,9 @@
 
 	NSArray *transientProperties = A(bounds);
 	NSArray *persistentProperties = A(path, pathResizeSel, fillColor, strokeColor, alpha, hidden);
+
+	[entity setUIBuilderPropertyNames: (id)[[A(path, fillColor, strokeColor,
+		alpha) mappedCollection] name]];
 
 	[[persistentProperties mappedCollection] setPersistent: YES];
 	[entity setPropertyDescriptions: [persistentProperties arrayByAddingObjectsFromArray: transientProperties]];
