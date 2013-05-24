724b038013740ff2b25bb4892f7e180517700cf2
Quentin Mathé <quentin.mathe@gmail.com>
2013-05-24 16:18:58 +0000

    Finished ETController model description
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@8371 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- ModelDescription/ETController+ModelDescription.m
+++ ModelDescription/ETController+ModelDescription.m
@@ -31,7 +31,7 @@
 
 @end
 
-
+// NOTE: ETDocumentController uses ETController model description
 @interface ETController (ModelDescription)
 @end
 
@@ -46,16 +46,97 @@
 	if ([[entity name] isEqual: [ETController className]] == NO) 
 		return entity;
 
+	/* Persistent Properties */
+
 	ETPropertyDescription *content = 
 		[ETPropertyDescription descriptionWithName: @"content" type: (id)@"ETLayoutItemGroup"];
 	[content setOpposite: (id)@"ETLayoutItemGroup.controller"];
+	ETPropertyDescription *templates =
+		[ETPropertyDescription descriptionWithName: @"templates" type: (id)@"ETItemTemplate"];
+	[templates setMultivalued: YES];
+	[templates setOrdered: NO];
+	ETPropertyDescription *currentObjectType =
+		[ETPropertyDescription descriptionWithName: @"currentObjectType" type: (id)@"ETUTI"];
+	ETPropertyDescription *currentGroupType =
+		[ETPropertyDescription descriptionWithName: @"currentGroupType" type: (id)@"ETUTI"];
+	[currentGroupType setReadOnly: YES];
+	// FIXME: Register a dummy class for the protocol COPersistentObjectContext
+	ETPropertyDescription *persistentObjectContext =
+		[ETPropertyDescription descriptionWithName: @"persistentObjectContext" type: (id)@"NSObject"];
+	ETPropertyDescription *clearsFilterPredicate =
+		[ETPropertyDescription descriptionWithName: @"clearsFilterPredicateOnInsertion" type: (id)@"BOOL"];
+	ETPropertyDescription *selectsInsertedObjects =
+		[ETPropertyDescription descriptionWithName: @"selectsInsertedObjects" type: (id)@"BOOL"];
+	ETPropertyDescription *sortDescriptors =
+		[ETPropertyDescription descriptionWithName: @"sortDescriptors" type: (id)@"NSSortDescriptor"];
+	[templates setMultivalued: YES];
+	[templates setOrdered: YES];
+	ETPropertyDescription *filterPredicate =
+		[ETPropertyDescription descriptionWithName: @"filterPredicate" type: (id)@"NSPredicate"];
+	ETPropertyDescription *automaticallyRearranges =
+		[ETPropertyDescription descriptionWithName: @"automaticallyRearrangesObjects" type: (id)@"BOOL"];
+	ETPropertyDescription *allowedPickTypes =
+		[ETPropertyDescription descriptionWithName: @"allowedPickTypes" type: (id)@"ETUTI"];
+	[allowedPickTypes setMultivalued: YES];
+	[allowedPickTypes setOrdered: YES];
+	ETPropertyDescription *allowedDropTypes =
+		[ETPropertyDescription descriptionWithName: @"allowedDropTypes" type: (id)@"ETUTI"];
+	[allowedDropTypes setMultivalued: YES];
+	[allowedDropTypes setOrdered: NO];
 
-	/* Transient ivars
-	   content (weak ref) */
+	/* Transient Properties */
+
+	ETPropertyDescription *nibMainContent =
+		[ETPropertyDescription descriptionWithName: @"nibMainContent" type: (id)@"NSObject"];
+	ETPropertyDescription *builder =
+		[ETPropertyDescription descriptionWithName: @"builder" type: (id)@"ETLayoutItemBuilder"];
+	ETPropertyDescription *nextResponder =
+		[ETPropertyDescription descriptionWithName: @"nextResponder" type: (id)@"NSObject"];
+	[nextResponder setReadOnly: YES];
+	ETPropertyDescription *defaultOptions =
+		[ETPropertyDescription descriptionWithName: @"defaultOptions" type: (id)@"NSObject"];
+	[defaultOptions setMultivalued: YES];
+	[defaultOptions setOrdered: NO];
+	[defaultOptions setReadOnly: YES];
+	ETPropertyDescription *canMutate =
+		[ETPropertyDescription descriptionWithName: @"canMutate" type: (id)@"BOOL"];
+	[canMutate setReadOnly: YES];
+	ETPropertyDescription *isContentMutable =
+		[ETPropertyDescription descriptionWithName: @"isContentMutable" type: (id)@"BOOL"];
+	[isContentMutable setReadOnly: YES];
+	ETPropertyDescription *insertionIndex =
+		[ETPropertyDescription descriptionWithName: @"insertionIndex" type: (id)@"NSInteger"];
+	[insertionIndex setReadOnly: YES];
+	ETPropertyDescription *insertionIndexPath =
+		[ETPropertyDescription descriptionWithName: @"insertionIndexPath" type: (id)@"NSIndexPath"];
+	[insertionIndexPath setReadOnly: YES];
+	ETPropertyDescription *additionIndexPath =
+		[ETPropertyDescription descriptionWithName: @"additionIndexPath" type: (id)@"NSIndexPath"];
+	[additionIndexPath setReadOnly: YES];
+	ETPropertyDescription *isEditing =
+		[ETPropertyDescription descriptionWithName: @"isEditing" type: (id)@"BOOL"];
+	[isEditing setReadOnly: YES];
+
+	NSArray *transientProperties = A(nibMainContent, builder, currentGroupType,
+		nextResponder, defaultOptions, canMutate, isContentMutable,
+		insertionIndex, insertionIndexPath, additionIndexPath, isEditing);
 	NSArray *persistentProperties = A(content);
+	// FIXME: Using all persistent properties is not yet tested...
+	NSArray *futurePersistentProperties = A(content, templates, currentObjectType,
+		persistentObjectContext, clearsFilterPredicate,
+		selectsInsertedObjects, sortDescriptors, filterPredicate,
+		automaticallyRearranges, allowedPickTypes, allowedDropTypes);
+	
+	transientProperties = [transientProperties arrayByAddingObjectsFromArray: futurePersistentProperties];
+
+	[entity setUIBuilderPropertyNames: (id)[[A(templates, currentObjectType,
+		currentGroupType, persistentObjectContext, clearsFilterPredicate,
+		selectsInsertedObjects, sortDescriptors, filterPredicate,
+		automaticallyRearranges, allowedPickTypes, allowedDropTypes) mappedCollection] name]];
 
 	[[persistentProperties mappedCollection] setPersistent: YES];
-	[entity setPropertyDescriptions: persistentProperties];
+	[entity setPropertyDescriptions:
+	 	[persistentProperties arrayByAddingObjectsFromArray: transientProperties]];
 
 	return entity;
 }
