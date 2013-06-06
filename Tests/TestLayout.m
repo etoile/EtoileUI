bbf745647bdb2bc32783985976b68fdf57200305
Quentin Mathé <quentin.mathe@gmail.com>
2013-06-06 17:37:24 +0000

    Added basic -isContentSizeLayout related tests
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileUI@8444 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/TestLayout.m
+++ Tests/TestLayout.m
@@ -6,10 +6,13 @@
 	License:  Modified BSD (see COPYING)
  */
  
-#import <Foundation/Foundation.h>
-#import <AppKit/AppKit.h>
-#import <UnitKit/UnitKit.h>
+#import "TestCommon.h"
+#import "ETColumnLayout.h"
 #import "ETLayout.h"
+#import "ETLayoutItem.h"
+#import "ETLayoutItemFactory.h"
+#import "ETLayoutItemGroup.h"
+#import "ETLayoutExecutor.h"
 #import "ETTableLayout.h"
 #import "ETCompatibility.h"
 
@@ -53,6 +56,68 @@
 	UKStringsEqual(@"layout", [self aspectName]);
 	UKStringsEqual(@"table", [ETTableLayout aspectName]);
 	UKStringsEqual(@"birdTableBird", [WXYBirdTableBirdBird aspectName]);
+}
+
+@end
+
+
+@interface TestPositionalLayout : NSObject <UKTest>
+{
+	ETLayoutItemFactory *itemFactory;
+	ETLayoutItemGroup *itemGroup;
+	ETLayoutItem *item;
+}
+
+@end
+
+@implementation  TestPositionalLayout
+
+- (id) init
+{
+	[[ETLayoutExecutor sharedInstance] removeAllItems];
+	SUPERINIT
+	ASSIGN(itemFactory, [ETLayoutItemFactory factory]);
+	ASSIGN(itemGroup, [itemFactory itemGroup]);
+	ASSIGN(item, [itemFactory item]);
+	return self;
+}
+
+- (void) dealloc
+{
+	DESTROY(itemFactory);
+	DESTROY(itemGroup);
+	DESTROY(item);
+	[super dealloc];
+}
+
+- (void) testUpsizedItemForContentSizeLayout
+{
+	ETColumnLayout *layout = [ETColumnLayout layout];
+	
+	[layout setIsContentSizeLayout: YES];
+	[itemGroup setLayout: layout];
+
+	ETLayoutItem *textItem = [itemFactory textField];
+
+	[item setWidth: 700];
+	[textItem setHeight: 500];
+
+	CGFloat width = MAX([item width], [textItem width]);
+	CGFloat height = [item height] + [textItem height];
+	NSSize contentSize = NSMakeSize(width, height);
+
+	[itemGroup addItems: A(item, textItem)];
+	
+	[[ETLayoutExecutor sharedInstance] execute];
+
+	UKSizesEqual(contentSize, [layout layoutSize]);
+	UKSizesEqual(contentSize, [itemGroup size]);
+}
+
+- (void) testDownsizedItemForContentSizeLayout
+{
+	ASSIGN(itemGroup, [itemFactory itemGroupWithSize: NSMakeSize(10000, 10000)]);
+	[self testUpsizedItemForContentSizeLayout];
 }
 
 @end
