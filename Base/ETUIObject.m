d3b97d1bb7d99adb0c23db2ee9e2937981cbdfa1
Quentin Mathé <qmathe@gmail.com>
2014-11-02 14:02:34 +0100

    Don't prevent -[COObject awakeFromDeserialization] to be called in ETUIObject subclasses

--- Base/ETUIObject.m
+++ Base/ETUIObject.m
@@ -179,12 +179,6 @@ see -[ETStyle setIsShared:]. */
 
 }*/
 
-// FIXME: Remove
-- (void) awakeFromDeserialization
-{
-    
-}
-
 - (BOOL) isCoreObjectReference: (id)value
 {
 	return ([value isKindOfClass: [ETUUID class]] || [value isKindOfClass: [COPath class]]);
